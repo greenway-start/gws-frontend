71e28a180be3cdc2e5456db05aa87cf3ac393d56
unknown <alisatpay@mail.ru>
2024-05-17 15:39:11 +0600

    Добавил If в books

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -2,24 +2,28 @@ import React from "react";
 import Book from "./Book";
 
 const Books = ({ books, onEdit, onDelete }) => {
+    if (books.length < 1) {
+        return (
+            <div className="book">
+                <h3>Книг нет</h3>
+            </div>
+        );
+    }
+
     return (
         <div>
-            {books.length > 0 ? (
-                books.map((el) => (
-                    <Book
-                        key={el.id}
-                        onEdit={onEdit}
-                        onDelete={onDelete}
-                        book={el}
-                    />
-                ))
-            ) : (
-                <div className="book">
-                    <h3>Книг нет</h3>
-                </div>
-            )}
+            {books.map((el) => (
+                <Book
+                    key={el.id}
+                    onEdit={onEdit}
+                    onDelete={onDelete}
+                    book={el}
+                />
+            ))}
         </div>
     );
 };
 
 export default Books;
+
+
